b9fa238e42b0ece4d807e8328a69cb104cba8dea
portdeveloper <108868128+portdeveloper@users.noreply.github.com>
2025-07-26 12:07:29 +0300

    Move breadcrumbs and hide text on smaller screens

--- src/CopyPageButton.js
+++ src/CopyPageButton.js
@@ -452,7 +452,7 @@ Please provide a clear summary and help me understand the key concepts covered i
             <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
             <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
           </svg>
-          Copy page
+          <span className={styles.copyPageText}>Copy page</span>
           <svg
             width="12"
             height="12"
